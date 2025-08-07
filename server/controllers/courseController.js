209654bdb4cc764942d68c1e76722d208ad22b14
Prasanna Achar <prasannaachar64@gmail.com>
2025-08-07 13:05:45 +0530

    coursecontroller.js

--- server/controllers/courseController.js
+++ server/controllers/courseController.js
@@ -2,38 +2,38 @@ import Course from "../models/Course.js";
 
 
 // get all courses
-export const getAllCourses = async (req ,res)=>{
+export const getAllCourses = async (req, res) => {
   try {
-    const courses = await Course.find({isPublished: true}).select(['-courseContent', '-enrolledStudents']).populate({path: 'educator'})
+    const courses = await Course.find({ isPublished: true }).select(['-courseContent', '-enrolledStudents']).populate({ path: 'educator' })
 
-    res.json({success: true, courses})
+    res.json({ success: true, courses })
 
   } catch (error) {
-    res.json({success: false , message:error.message})
+    res.json({ success: false, message: error.message })
   }
 }
 
 // get course by id
-export const  getCourseId = async (req , res)=>{
-   const {id} = req.params
+export const getCourseId = async (req, res) => {
+  const { id } = req.params
+  const userId = req.user?._id || req.userId;
 
+  try {
+    const courseData = await Course.findById(id).populate({ path: 'educator' })
 
-    try {
-       const courseData = await Course.findById(id).populate({path:'educator'})
-       
     //    remove lectireurl if ispreviewfree is false
     courseData.courseContent.forEach(chapter => {
-        chapter.chapterContent.forEach(lecture => {
-            if (!lecture.isPreviewFree) {
-                lecture.lectureUrl = "";
-            }
-        })
+      chapter.chapterContent.forEach(lecture => {
+        if (!lecture.isPreviewFree) {
+          lecture.lectureUrl = "";
+        }
+      })
     })
-    
-    res.json({success: true, courseData})
-    } catch (error) {
-            res.json({success: false , message:error.message})
-    }
+
+    res.json({ success: true, courseData })
+  } catch (error) {
+    res.json({ success: false, message: error.message })
+  }
 }
 
 
